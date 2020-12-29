ab2cfb4d5fd5d475384bec31c1231205213c390b
Luciana <lucianaqueiroz098@hotmail.com>
2020-12-29 13:22:33 -0300

    teste index

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
-
+import Button from "@material-ui/Button"
+import TextField from "@material-ui/core/TextField"
 function FormularioCadastro(){
 
     //toda função tem que ter um retorno
     return(
         <form>
+            <TextField id="nome" label="Nome"/>
+            
+
+            <label>Sobrenome</label>
             <input type="text"/>
+
+            <label>cpf</label>
+            <input type="text"/>
+
+            <label>Promoções</label>
+            <input type="checkbox"/>
+
+            <label>Novidades</label>
+            <input type="checkbox"/>
+
+            <Button variant="contained" color="primary"  type="submit">Cadastrar</Button>
+
         </form>
     );
 
 }
+
+//npm install @material-ui/core
 
 export default FormularioCadastro ;
--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+
+
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
